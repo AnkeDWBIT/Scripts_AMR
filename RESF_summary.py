3760c772517a41e523a9e7962f9fcae4bb6b8239
AnkeDW_BIT <anke.de.wispelaere@student.howest.be>
2024-08-13 03:23:25 -0400

    Sort input folder chronologically, removed file closing & added print message at the end.

--- RESF_summary.py
+++ RESF_summary.py
@@ -26,50 +26,50 @@ header = ["File_ID"] + AB_list
 ws.write_row(0, 0, header) 
 
 excel_line = 2
+
 # Look in the input directory (ResFinder results) for subfolders (samples/strains)
-pheno_data = []
-sample_names = []
-for subdir in os.listdir(input_dir):
+# Sort the subfolders to make sure the order is consistent
+subdirs = sorted([d for d in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, d))])
+
+for subdir in subdirs:
     subdir_path = os.path.join(input_dir, subdir)
-    if os.path.isdir(subdir_path):
-        # Look through all the files and search the .txt files with the phenotypic data
-        for filename in os.listdir(subdir_path):
-            if filename == data_file:
-                file_path = os.path.join(subdir_path, filename)
-                #print(f"Found phenotypic data for sample {subdir} in file {filename}")
+    # Look through all the files and search the .txt files with the phenotypic data
+    for filename in os.listdir(subdir_path):
+        if filename == data_file:
+            file_path = os.path.join(subdir_path, filename)
+            #print(f"Found phenotypic data for sample {subdir} in file {filename}")
 
-                 # Make a list to store the phenotypic data
-                pheno_data = [subdir]
+            # Make a list to store the phenotypic data
+            pheno_data = [subdir]
 
-                # Read the text file and extract information
-                with open(file_path, 'r') as file:
-                    lines = file.readlines()
-                    # Iterate through the lines
-                    for AB in AB_list:
-                        for line in lines:
-                            # Split the line into columns
-                            parts = line.split('\t')
-                            if len(parts) >= 4:
-                                antibiotic = parts[0].strip()
-                                phenotype = parts[2].strip()
-                                # If  a line contains the antibiotic of interest, extract the resistance phenotype
-                                if AB == antibiotic:
-                                    if parts[2].strip() == "No resistance":
-                                        pheno_data.append("S")
-                                    if parts[2].strip() == "Resistant":
-                                        pheno_data.append("R")
+            # Read the text file and extract information
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                # Iterate through the lines
+                for AB in AB_list:
+                    for line in lines:
+                        # Split the line into columns
+                        parts = line.split('\t')
+                        if len(parts) >= 4:
+                            antibiotic = parts[0].strip()
+                            phenotype = parts[2].strip()
+                            # If  a line contains the antibiotic of interest, extract the resistance phenotype
+                            if AB == antibiotic:
+                                if parts[2].strip() == "No resistance":
+                                    pheno_data.append("S")
+                                if parts[2].strip() == "Resistant":
+                                    pheno_data.append("R")
+
+                # Write the phenotypic data from the strain to the Excel file
+                ws.write_row(excel_line, 0, pheno_data)
+
+                # Add to the Excel line counter
+                excel_line += 1
                 
-
-                    # Write the phenotypic data from the strain to the Excel file
-                    ws.write_row(excel_line, 0, pheno_data)
-
-                    # Add to the Excel line counter
-                    excel_line += 1
-                    
-                # Close the file
-                file.close()
+                # 'With' statement automatically closes the file after the block of code is finished
 
 # Close the workbook
-wb.close()                            
+wb.close()                     
                         
-
+# Print a message to indicate the script has finished
+print("Summary Excel file has been created successfully!")
