c04ee5db8096db3b8e58347f41c44c0b2fc89191
AnkeDW_BIT <anke.de.wispelaere@student.howest.be>
2024-08-19 03:10:00 -0400

    Fixed updating of CARD column, select correct worksheet.

--- update_AMRF_CARD.py
+++ update_AMRF_CARD.py
@@ -70,37 +70,37 @@ while row <= AMRF_ws.max_row:
 row = 3
 while row <= AMRF_ws.max_row:
     # Read values from specific cells
-    value_2 = AMRF_ws.cell(row=row, column=2).value
-    value_3 = AMRF_ws.cell(row=row, column=3).value
-    value_4 = AMRF_ws.cell(row=row, column=4).value
-    value_5 = AMRF_ws.cell(row=row, column=5).value
-    value_6 = AMRF_ws.cell(row=row, column=6).value
-    value_7 = AMRF_ws.cell(row=row, column=7).value
-    value_8 = AMRF_ws.cell(row=row, column=8).value
-    value_9 = AMRF_ws.cell(row=row, column=9).value
-    value_10 = AMRF_ws.cell(row=row, column=10).value
+    val_2 = CARD_ws.cell(row=row, column=2).value
+    val_3 = CARD_ws.cell(row=row, column=3).value
+    val_4 = CARD_ws.cell(row=row, column=4).value
+    val_5 = CARD_ws.cell(row=row, column=5).value
+    val_6 = CARD_ws.cell(row=row, column=6).value
+    val_7 = CARD_ws.cell(row=row, column=7).value
+    val_8 = CARD_ws.cell(row=row, column=8).value
+    val_9 = CARD_ws.cell(row=row, column=9).value
+    val_10 = CARD_ws.cell(row=row, column=10).value
     # Row 11 is skipped as it contains results for piperacillin, but this AB is not present on it's own in the output file
-    value_12 = AMRF_ws.cell(row=row, column=12).value
-    value_13 = AMRF_ws.cell(row=row, column=13).value
-    value_14 = AMRF_ws.cell(row=row, column=14).value
-    value_15 = AMRF_ws.cell(row=row, column=15).value
-    value_16 = AMRF_ws.cell(row=row, column=16).value
+    val_12 = CARD_ws.cell(row=row, column=12).value
+    val_13 = CARD_ws.cell(row=row, column=13).value
+    val_14 = CARD_ws.cell(row=row, column=14).value
+    val_15 = CARD_ws.cell(row=row, column=15).value
+    val_16 = CARD_ws.cell(row=row, column=16).value
 
     # Write these values to specific cells in the output file
-    output_ws.cell(row=row, column=6).value = value_2
-    output_ws.cell(row=row, column=13).value = value_3
-    output_ws.cell(row=row, column=20).value = value_4
-    output_ws.cell(row=row, column=27).value = value_5
-    output_ws.cell(row=row, column=34).value = value_6
-    output_ws.cell(row=row, column=41).value = value_7
-    output_ws.cell(row=row, column=48).value = value_8
-    output_ws.cell(row=row, column=58).value = value_9
-    output_ws.cell(row=row, column=62).value = value_10
-    output_ws.cell(row=row, column=69).value = value_12
-    output_ws.cell(row=row, column=76).value = value_13
-    output_ws.cell(row=row, column=83).value = value_14
+    output_ws.cell(row=row, column=6).value = val_2
+    output_ws.cell(row=row, column=13).value = val_3
+    output_ws.cell(row=row, column=20).value = val_4
+    output_ws.cell(row=row, column=27).value = val_5
+    output_ws.cell(row=row, column=34).value = val_6
+    output_ws.cell(row=row, column=41).value = val_7
+    output_ws.cell(row=row, column=48).value = val_8
+    output_ws.cell(row=row, column=58).value = val_9
+    output_ws.cell(row=row, column=62).value = val_10
+    output_ws.cell(row=row, column=69).value = val_12
+    output_ws.cell(row=row, column=76).value = val_13
+    output_ws.cell(row=row, column=83).value = val_14
     # Only if row 15 (trimethoprim) & 16 (sulfamethoxazole) are both "R", then write "R" to the output file
-    if value_15 == "R" and value_16 == "R":
+    if val_15 == "R" and val_16 == "R":
         output_ws.cell(row=row, column=90).value = "R"
     else:
         output_ws.cell(row=row, column=90).value = "S"
