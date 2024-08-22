b5e13f1a00007ce3e8cb516e18fb20ca862b915c
AnkeDW_BIT <anke.de.wispelaere@student.howest.be>
2024-08-22 06:26:10 -0400

    Added results from Resistance-ResultsTableMut.tsv to reference list.

--- BN_reflist.py
+++ BN_reflist.py
@@ -8,11 +8,11 @@ import openpyxl
 
 # Define the input-dir & output-file
 input_dir = "/home/guest/BIT11_Traineeship/Ecoli_AMR/BN_data/"
-output_file = "/home/guest/BIT11_Traineeship/Ecoli_AMR/BN_reflist.xlsx"
+output_file = "/home/guest/BIT11_Traineeship/Ecoli_AMR/Reference_lists/BN_reflist.xlsx"
 
 # STEP 1 : Retrieve data from the input_dir and save it in a dictionary
 ############################################################################################################################################################
-# Loop through the input directory and look for the files named "Resistance_ResultsTableAcq.tsv"
+# Loop through the input directory and look for the files named "Resistance_ResultsTableAcq.tsv" & "Resistance_ResultsTableMut.tsv"
 data_dict = {}
 for subdir, dirs, files in os.walk(input_dir):
     sample = os.path.basename(subdir)
@@ -39,7 +39,28 @@ for subdir, dirs, files in os.walk(input_dir):
                             data_dict[gene].append(antibiotic)
                         else:
                             continue                                        # If gene & AB are present : continue
-
+        if file == "Resistance-ResultsTableMut.tsv":
+            file_path = os.path.join(subdir, file) 
+            # Read the file
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+                # Iterate through the lines, skipping the first line
+                for i, line in enumerate(lines):
+                    if i == 0:
+                        continue
+                    # Split the line into columns
+                    parts = line.split('\t')
+                    gene = parts[1].strip()
+                    antibiotic = parts[0].strip()
+                    # Add the gene & antibiotic to the dictionary
+                    if gene not in data_dict:
+                        data_dict[gene] = [antibiotic]
+                    else:
+                        if antibiotic not in data_dict[gene]:
+                            data_dict[gene].append(antibiotic)
+                        else:
+                            continue
+                               
 #print(data_dict)
                         
 # STEP 2 : Create an Excel file to store the reference list
